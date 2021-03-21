cfb3078de09485814fe9acfd399a98ec78f1840f
FjnuWyh <1071495037@qq.com>
2021-03-21 23:18:17 +0800

    提交网页前端UI项目

---
+++ present-cloud-web/src/views/dashboard/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="dashboard-container">
+    <div class="dashboard-text">欢迎{{username}}使用到云管理系统</div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'Dashboard',
+  data() {
+    return {
+      username: localStorage.getItem('username')
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'name'
+    ])
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
